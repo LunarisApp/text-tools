a824b24c0e5d1ece61abdab79bb7b63e12f786fa
MrLightful <romatallinn@gmail.com>
2025-02-13 16:11:57 +0100

    Remove compiled

--- packages/textstat/src/config.d.ts
+++
@@ -1,7 +0,0 @@
-export type LangConfig = {
-  fre_base: number;
-  fre_sentence_length: number;
-  fre_syllables_per_word: number;
-  syllable_threshold?: number;
-};
-export declare const langs: Record<string, LangConfig>;
